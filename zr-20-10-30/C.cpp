26c5bcbc442da1f4ec71fad0da1d1067e7967140
green_orange <yangjz1125@outlook.com>
2020-10-30 17:57:17 +0800

    Finish C and Modify .gitignore.

--- zr-20-10-30/C.cpp
+++ zr-20-10-30/C.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 typedef long long inte;
 
-const static int maxn = 1e5 + 1;
-const static inte mods = 1000000007;
+const static int maxn = 2e5 + 1;
+const static inte mods = 1e9 + 7;
 
 inte fpow(inte x, int p)
 {
@@ -19,11 +19,9 @@ inte fpow(inte x, int p)
     return res;
 }
 
-inte f[maxn * 2], g[maxn * 2];
 int K, N, M;
 int L[maxn * 2][2], R[maxn * 2][2];
-int pre[maxn * 2][2];
-bool vis[maxn * 2][2];
+int ll[maxn * 2][2];
 
 int plc[maxn * 2], pc;
 
@@ -32,10 +30,19 @@ inline int getid(int x)
     return lower_bound(plc + 1, plc + pc + 1, x) - plc;
 }
 
+inline int& letmax(int& l, int r)
+{
+    return (l < r ? l = r : l);
+}
+
+inte f[maxn * 2], g[maxn * 2], h[maxn * 2];
+
+inte sf[maxn * 2], sg[maxn * 2], sh[maxn * 2];
+
 int main()
 {
     cin >> K >> N >> M;
-    plc[++pc] = K;
+    plc[++pc] = K, plc[++pc] = 0;
     for (int i = 1; i <= N; ++i)
         cin >> L[i][0] >> R[i][0], plc[++pc] = L[i][0] - 1, plc[++pc] = R[i][0];
     for (int i = 1; i <= M; ++i)
@@ -43,29 +50,27 @@ int main()
     sort(plc + 1, plc + pc + 1);
     pc = unique(plc + 1, plc + pc + 1) - plc - 1;
     for (int i = 1; i <= N; ++i) {
-        L[i][0] = getid(L[i][0]), R[i][0] = getid(R[i][0]);
-        ++pre[L[i][0] + 1][0], --pre[R[i][0] + 1][0];
+        R[i][0] = getid(R[i][0]);
+        L[i][0] = getid(L[i][0] - 1);
+        letmax(ll[R[i][0]][0], L[i][0] + 1);
     }
     for (int i = 1; i <= M; ++i) {
-        L[i][1] = getid(L[i][1]), R[i][1] = getid(R[i][1]);
-        ++pre[L[i][1] + 1][1], --pre[R[i][1] - 1][1];
+        R[i][1] = getid(R[i][1]);
+        L[i][1] = getid(L[i][1] - 1);
+        letmax(ll[R[i][1]][1], L[i][1] + 1);
     }
-    for (int j = 0; j <= 1; ++j) {
-        int acm = 0;
-        for (int i = 1; i <= pc; ++i) {
-            acm += pre[i][j];
-            if (acm)
-                vis[i][j] = 1;
-        }
+    h[1] = sh[1] = ll[1][0] = ll[1][1] = 1;
+    for (int i = 2; i <= pc; ++i) {
+        ll[i][0] = max(ll[i][0], ll[i - 1][0]);
+        ll[i][1] = max(ll[i][1], ll[i - 1][1]);
+        f[i] = ((sg[i - 1] - sg[ll[i][1] - 1]) + sh[i - 1] - sh[ll[i][1] - 1]) % mods;
+        g[i] = ((sf[i - 1] - sf[ll[i][0] - 1]) + sh[i - 1] - sh[ll[i][0] - 1]) % mods;
+        h[i] = (f[i - 1] + g[i - 1] + h[i - 1]) % mods * (fpow(2, plc[i] - plc[i - 1]) - 2 + mods) % mods;
+        sf[i] = (sf[i - 1] + f[i]) % mods;
+        sg[i] = (sg[i - 1] + g[i]) % mods;
+        sh[i] = (sh[i - 1] + h[i]) % mods;
     }
-    f[0] = 0;
-    g[0] = 1;
-    for (int i = 1; i <= pc; ++i) {
-        f[i] = (f[i - 1] * fpow(2, plc[i] - plc[i - 1]) % mods + g[i - 1] * (vis[i][0] + vis[i][1])) % mods;
-        g[i] = (fpow(2, plc[i]) - f[i]) % mods;
-        cout << "Attention" << i << ' ' << plc[i] << endl;
-        cout << f[i] << ' ' << g[i] << endl;
-    }
-    cout << f[pc] << endl;
+    // OAcopy(f + 1, f + pc + 1, ostream_iterator<inte>(cout, " "));
+    cout << ((f[pc] + g[pc] + h[pc]) % mods + mods) % mods;
     return 0;
 }
