69fd001e0d8e6a5d33e9fe19fbcd4c5a6ae77720
green_orange <yangjz1125@outlook.com>
2020-11-22 21:55:02 +0800

    Finish Luogu 4168

---
+++ luogu-4168.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int maxn = 40001, maxsqn = 202, blksiz = 200;
+int plc[maxn], A[maxn];
+struct Solution {
+    int id, v;
+    Solution() : id(0), v(0) {};
+    Solution(int _id, int _v) : id(_id), v(_v) {}
+};
+bool operator < (const Solution& a, const Solution& b)
+{return a.v == b.v ? a.id > b.id : a.v < b.v;}
+int pre[maxn][maxsqn];
+Solution most[maxsqn][maxsqn];
+int N, M;
+int getBlock(int x) {return x / blksiz;}
+int cnt[maxn] = {};
+template<typename T>
+inline T& letmax(T& l, const T& r) {return l < r ? l = r : l;}
+int main() {
+    cin >> N >> M;
+    copy_n(istream_iterator<int>(cin), N, A + 1);
+    copy(A, A + N, plc);
+    sort(plc, plc + N);
+    transform(A, A + N, A, [&](int x){return lower_bound(plc, plc + N, x) - plc;});
+    const int bsize = getBlock(N);
+    for (int i = 1; i <= N; ++i)
+        ++pre[A[i]][getBlock(i)];
+    for (int i = 1; i <= N; ++i)
+        partial_sum(pre[i], pre[i] + bsize + 1, pre[i] + 1);
+    for (int i = 0; i <= bsize; ++i) {
+        for (int j = blksiz * i; j < N; ++j) {
+            ++cnt[A[j]];
+            letmax(most[i][getBlock(j)], Solution(A[j], cnt[A[j]]));
+        }
+        for (int j = blksiz * i; j < N; ++j)
+            cnt[A[j]] = 0;
+    }
+    int x = 0;
+    for (int i = 1; i <= M; ++i) {
+        int l, r;
+        cin >> l >> r;
+        l = (l + x - 1) % N + 1;
+        r = (r + x - 1) % N + 1;
+        if (l > r)
+            swap(l, r);
+        const int lb = getBlock(l) + 1, rb = getBlock(r) - 1;
+        Solution res;
+        if (lb <= rb) {
+            res = most[lb][rb];
+            for (int i = l; getBlock(i) != lb; ++i)
+                letmax(res, Solution(A[i], ++cnt[A[i]] + pre[A[i]][rb] - pre[A[i]][lb - 1]));
+            for (int i = r; getBlock(i) != rb; --i)
+                letmax(res, Solution(A[i], ++cnt[A[i]] + pre[A[i]][rb] - pre[A[i]][lb - 1]));
+            for (int i = l; getBlock(i) != lb; ++i)
+                cnt[A[i]] = 0;
+            for (int i = r; getBlock(i) != rb; --i)
+                cnt[A[i]] = 0;
+        } else {
+            for (int i = l; i <= r; ++i)
+                letmax(res, Solution(A[i], ++cnt[A[i]]));
+            for (int i = l; i <= r; ++i)
+                cnt[A[i]] = 0;
+        }
+        cout << res.id << '\n';
+        x = res.id;
+    }
+    return 0;
+}
