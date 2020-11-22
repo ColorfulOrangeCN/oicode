64e19f4a49fe219c894a84547aed0f796bde36ee
green_orange <yangjz1125@outlook.com>
2020-11-22 23:10:37 +0800

    Modify luogu 4168

--- luogu-4168.cpp
+++ luogu-4168.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int maxn = 40001, maxsqn = 202, blksiz = 200;
+constexpr int maxn = 50001, maxsqn = 202, blksiz = 200;
 int plc[maxn], A[maxn];
 struct Solution {
     int id, v;
@@ -18,21 +18,21 @@ int cnt[maxn] = {};
 template<typename T>
 inline T& letmax(T& l, const T& r) {return l < r ? l = r : l;}
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     cin >> N >> M;
-    copy_n(istream_iterator<int>(cin), N, A + 1);
+    copy_n(istream_iterator<int>(cin), N, A);
     copy(A, A + N, plc);
     sort(plc, plc + N);
     transform(A, A + N, A, [&](int x){return lower_bound(plc, plc + N, x) - plc;});
-    const int bsize = getBlock(N);
-    for (int i = 1; i <= N; ++i)
+    const int bsize = getBlock(N - 1);
+    for (int i = 0; i < N; ++i)
         ++pre[A[i]][getBlock(i)];
-    for (int i = 1; i <= N; ++i)
-        partial_sum(pre[i], pre[i] + bsize + 1, pre[i] + 1);
+    for (int i = 0; i < N; ++i)
+        partial_sum(pre[i], pre[i] + bsize + 1, pre[i]);
     for (int i = 0; i <= bsize; ++i) {
-        for (int j = blksiz * i; j < N; ++j) {
-            ++cnt[A[j]];
-            letmax(most[i][getBlock(j)], Solution(A[j], cnt[A[j]]));
-        }
+        for (int j = blksiz * i; j < N; ++j)
+            letmax(most[i][getBlock(j)], Solution(A[j], ++cnt[A[j]]));
         for (int j = blksiz * i; j < N; ++j)
             cnt[A[j]] = 0;
     }
@@ -44,9 +44,11 @@ int main() {
         r = (r + x - 1) % N + 1;
         if (l > r)
             swap(l, r);
+        --l, --r;
         const int lb = getBlock(l) + 1, rb = getBlock(r) - 1;
         Solution res;
         if (lb <= rb) {
+            //cerr << "DEBUG" << endl;
             res = most[lb][rb];
             for (int i = l; getBlock(i) != lb; ++i)
                 letmax(res, Solution(A[i], ++cnt[A[i]] + pre[A[i]][rb] - pre[A[i]][lb - 1]));
@@ -62,8 +64,8 @@ int main() {
             for (int i = l; i <= r; ++i)
                 cnt[A[i]] = 0;
         }
-        cout << res.id << '\n';
-        x = res.id;
+        cout << plc[res.id] << '\n';
+        x = plc[res.id];
     }
     return 0;
 }
